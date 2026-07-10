a7f37e85869443ce33cb0df54eaa36c1588d5bb6
Aaysha Agarwal <aaysha2103@gmail.com>
2026-07-10 23:10:25 +0530

    Time: 2 ms (76.44%), Space: 42.4 MB (99.44%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char c : s.toCharArray()) count[c]++;
+        int length = 0;
+        for (int c : count) {
+            length += c / 2 * 2;
+            if (length % 2 == 0 && c % 2 == 1) length++;
+        }
+        return length;
+    }
+}
