ac543f6c25aeb70810d0a62ac43687239a590b69
Aaysha Agarwal <aaysha2103@gmail.com>
2026-07-20 21:12:09 +0530

    Time: 13 ms (94.17%), Space: 108.8 MB (8.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int n : nums) {
+            if (!seen.add(n)) return true;
+        }
+        return false;
+    }
+}
